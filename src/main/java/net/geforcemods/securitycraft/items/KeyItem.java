47e88997d312e8dae2f6430d01c10ba0a25ad0c2
ilya814 <ilyatascken@gmail.com>
2026-03-01 19:46:35 +0100

    Add src/main/java/net/geforcemods/securitycraft/items/KeyItem.java

---
+++ src/main/java/net/geforcemods/securitycraft/items/KeyItem.java
@@ -0,0 +1,21 @@
+package net.geforcemods.securitycraft.items;
+
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.tooltip.TooltipType;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
+
+import java.util.List;
+
+public class KeyItem extends Item {
+
+    public KeyItem(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, TooltipContext context, List<Text> tooltip, TooltipType type) {
+        tooltip.add(Text.translatable("tooltip.securitycraft.key").formatted(Formatting.GRAY));
+    }
+}
