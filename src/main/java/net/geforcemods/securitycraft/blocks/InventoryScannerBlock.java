80f792e7492be5baf53e6f6a33dee284fcebe66a
ilya814 <ilyatascken@gmail.com>
2026-03-01 19:45:58 +0100

    Add src/main/java/net/geforcemods/securitycraft/blocks/InventoryScannerBlock.java

---
+++ src/main/java/net/geforcemods/securitycraft/blocks/InventoryScannerBlock.java
@@ -0,0 +1,54 @@
+package net.geforcemods.securitycraft.blocks;
+
+import net.geforcemods.securitycraft.SCContent;
+import net.geforcemods.securitycraft.blockentities.InventoryScannerBlockEntity;
+import net.minecraft.block.BlockRenderType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.entity.BlockEntityTicker;
+import net.minecraft.block.entity.BlockEntityType;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+public class InventoryScannerBlock extends OwnableBlock {
+
+    public InventoryScannerBlock(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player,
+                               BlockHitResult hit) {
+        if (world.isClient) return ActionResult.SUCCESS;
+        if (world.getBlockEntity(pos) instanceof InventoryScannerBlockEntity be) {
+            if (!be.isOwnedBy(player)) {
+                player.sendMessage(net.minecraft.text.Text.translatable("message.securitycraft.not_owner"), true);
+                return ActionResult.FAIL;
+            }
+            if (player instanceof ServerPlayerEntity sp) {
+                SCContent.openInventoryScannerScreen(sp, pos);
+            }
+        }
+        return ActionResult.SUCCESS;
+    }
+
+    @Override
+    public BlockRenderType getRenderType(BlockState state) {
+        return BlockRenderType.MODEL;
+    }
+
+    @Override
+    public BlockEntity createBlockEntity(BlockPos pos, BlockState state) {
+        return new InventoryScannerBlockEntity(pos, state);
+    }
+
+    @Override
+    public <T extends BlockEntity> BlockEntityTicker<T> getTicker(World world, BlockState state, BlockEntityType<T> type) {
+        if (world.isClient) return null;
+        return SentryBlock.checkType(type, SCContent.INVENTORY_SCANNER_BLOCK_ENTITY, InventoryScannerBlockEntity::tick);
+    }
+}
